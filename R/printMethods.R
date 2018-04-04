1ff95248811dc6b49b1184c8048f15bb78f67572
Ignacio <ignacio.gonzalez@bbox.fr>
2018-04-04 10:16:06 +0200

    Minor changes.

--- R/printMethods.R
+++ R/printMethods.R
@@ -3,9 +3,9 @@
 ##-----------------------------------------------------------------------------#
 print.missPattern <- function(x, ...) {
     
-    cat("Number of missing/available rows in ")
+    cat("Number of missing/available individuals in ")
     cat("each stratum per data table.\n")
-    cat("Table:\n")
+    cat("Tables:\n")
     print(x$nbMissing)
 }
 
