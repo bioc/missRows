964d79f2ec07bddc6591617c8be00bea60f62ba8
Ignacio <ignacio.gonzalez@bbox.fr>
2018-04-04 10:32:25 +0200

    Updating tests for the MIDTList constructor.

--- tests/testthat/test-MIDTList.R
+++ tests/testthat/test-MIDTList.R
@@ -3,41 +3,50 @@ context("test class constructors")
 data(NCI60)
 
 test_that("MIDTList object from a list of data tables", {
-    object <- newMIDTList(NCI60$incompleteData, strata=NCI60$cell.line)
-    expect_is(object, "MIDTList")
+    tableList <- NCI60$dataTables[1:2]
+    colData <- NCI60$dataTables$cell.line
+    
+    midt <- newMIDTList(tableList, colData=colData, 
+                        assayNames=c("trans", "prote"))
+    expect_is(midt, "MIDTList")
 })
 
 test_that("MIDTList object from separate data tables", {
-    table1 <- NCI60$incompleteData$trans
-    table2 <- NCI60$incompleteData$prote
+    table1 <- NCI60$dataTables$trans
+    table2 <- NCI60$dataTables$prote
+    colData <- NCI60$dataTables$cell.line
     
-    object <- newMIDTList(table1, table2, strata=NCI60$cell.line)
-    expect_is(object, "MIDTList")
+    midt <- newMIDTList(table1, table2, colData=colData,
+                        assayNames=c("transcrip", "proteome"))
+    expect_is(midt, "MIDTList")
+})
+
+test_that("MIDTList object from 'MultiAssayExperiment'", {
+    midt <- newMIDTList(NCI60$mae)
+    expect_is(midt, "MIDTList")
 })
 
 
 context("tests on inputs")
 
 test_that("tests for data tables", {
-    table1 <- NCI60$incompleteData$trans
-    table2 <- NCI60$incompleteData$prote
+    table1 <- NCI60$dataTables$trans
+    table2 <- NCI60$dataTables$prote
+    colData <- NCI60$dataTables$cell.line
     
-    expect_error(newMIDTList(table1, strata=NCI60$cell.line), 
-                "at least two data tables must be passed as arguments in '...'")
-    
-    expect_error(newMIDTList(table1, table2[-1, ], strata=NCI60$cell.line), 
-                "non equal row numbers among tables and/or strata")
+    expect_error(newMIDTList(table1, colData=colData), 
+            "at least two data tables must be passed as arguments in '...'")
     
     tableNrn <- table1
-    rownames(tableNrn) <- NULL
-    expect_error(newMIDTList(tableNrn, table2, strata=NCI60$cell.line), 
-                "the 'Table 1' data table must be rows named.")
+    colnames(tableNrn) <- NULL
+    expect_error(newMIDTList(tableNrn, table2, colData=colData), 
+                "the 'Table 1' data table must be columns named.")
     
-    expect_error(newMIDTList(1:nrow(table1), table2, strata=NCI60$cell.line), 
+    expect_error(newMIDTList(1:nrow(table1), table2, colData=colData), 
                 "the 'Table 1' data table must be a matrix or data frame.")
     
     tableInf <- table1
     tableInf[1, 1] <- Inf
-    expect_error(newMIDTList(tableInf, table2, strata=NCI60$cell.line), 
+    expect_error(newMIDTList(tableInf, table2, colData=colData), 
                  "infinite values in 'Table 1' data table.")
 })
