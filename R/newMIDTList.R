3362abee25e9f1b9654373e2707a68454e3c40b9
Ignacio <ignacio.gonzalez@bbox.fr>
2018-04-04 09:40:46 +0200

    Major changes. Now newMIDTList generates a subclass from MultiAssayExperiment.

--- R/newMIDTList.R
+++ R/newMIDTList.R
@@ -1,43 +1,89 @@
-newMIDTList <- function(..., strata=NULL, tableNames=NULL) {
+newMIDTList <- function(..., object) UseMethod("newMIDTList")
+
+
+##- newMIDTList default method -----------------------------------------------#
+##----------------------------------------------------------------------------#
+newMIDTList.default <- function(..., colData=NULL, strata=NULL, 
+                                assayNames=NULL) {
     
     ##- checking general input arguments -------------------------------------#
     ##------------------------------------------------------------------------#
     
     ##- input tables
     K <- list(...)
-    if (is.list(K[[1]])) { K <- K[[1]] }
     
+    if (length(K) == 1) {
+        K <- K[[1]]
+
+        if (!is.list(K) || is.data.frame(K)) {
+            stop("at least two data tables must be passed as arguments",
+                " in '...'", call.=FALSE)
+        }
+    }
+
     if (length(K) <= 1) {
         stop("at least two data tables must be passed as arguments in '...'",
             call.=FALSE)
     }
     
-    ##- strata
-    if (is.null(strata)) {
-        stop("'strata' must be a named vector or data frame", call.=FALSE)
+    ##- colData
+    if (is.null(colData)) {
+        stop("'colData' must be a data frame", call.=FALSE)
     }
     
-    if (is.data.frame(strata)) {
-        tmp <- rownames(strata)
-        strata <- strata[, 1]
-        names(strata) <- tmp
+    if (!is.data.frame(colData)) {
+        stop("'colData' must be a data frame", call.=FALSE)
+    } else {
+        tmp <- rownames(colData)
+        
+        if (is.null(tmp)) {
+            stop("'colData' must be a data frame with row names.", call.=FALSE)
+        }
+        
+        colData <- DataFrame(strata=colData[, 1], row.names=tmp)
+    }
+    
+    ##- strata
+    if (is.null(strata) & (ncol(colData) == 1)) {
+        strata <- colnames(colData)
+    }
+    
+    if (is.null(strata)) {
+        strata <- colnames(colData) <- "strata"
+    }
+    
+    if (length(strata) > 1) {
+        stop("'strata' must be a vector of length 1.", call.=FALSE)
+    }
+    
+    if (is.character(strata)) {
+        cond <- (strata %in% colnames(colData))
+        
+        if (!cond || length(cond) == 0) {
+            stop("'strata' do not match colnames in colData.", call.=FALSE)
+        }
+    } else {
+        if (strata %in% seq_len(ncol(colData))) {
+            stop("'strata' must be a integer value less than or equal to ", 
+                ncol(colData), call.=FALSE)
+        }
     }
     
     ##- table names
-    if (is.null(tableNames)) { tableNames <- names(K) }
+    if (is.null(assayNames)) { assayNames <- names(K) }
     
-    if (is.null(tableNames)) { tableNames <- paste0("Table ", seq_along(K)) }
+    if (is.null(assayNames)) { assayNames <- paste0("Table ", seq_along(K)) }
     
-    if (length(tableNames) != length(K)) {
-        stop("the length of 'tableNames' must be equal to ", length(K),
+    if (length(assayNames) != length(K)) {
+        stop("the length of 'assayNames' must be equal to ", length(K),
             call.=FALSE)
     }
     
-    if (any(duplicated(tableNames))) {
-        stop("non-unique values in 'tableNames'", call.=FALSE)
+    if (any(duplicated(assayNames))) {
+        stop("non-unique names in 'assayNames'", call.=FALSE)
     }
     
-    names(K) <- tableNames
+    names(K) <- assayNames
     
     ##- the individual tables
     for (i in seq_along(K)) {
@@ -46,7 +92,9 @@ newMIDTList <- function(..., strata=NULL, tableNames=NULL) {
                 " data frame.", call.=FALSE)
         }
         
-        if (!is.numeric(as.matrix(K[[i]]))) {
+        K[[i]] <- as.matrix(K[[i]])
+        
+        if (!is.numeric(K[[i]])) {
             stop("the '", names(K)[i], "' data table must be a matrix or",
                 " data frame.", call.=FALSE)
         }
@@ -60,52 +108,122 @@ newMIDTList <- function(..., strata=NULL, tableNames=NULL) {
         }
     }
     
-    ##- equal row numbers among tables
-    nr <- c(vapply(K, nrow, 1L), length(strata))
-    if (length(unique(nr)) != 1)
-        stop("non equal row numbers among tables and/or strata", 
-            call.=FALSE)
-    
-    ##- the tables are rows named
+    ##- the tables are columns named
     for (i in seq_along(K)) {
-        if (is.null(rownames(K[[i]]))) {
-            stop("the '", names(K)[i], "' data table must be rows named.", 
+        if (is.null(colnames(K[[i]]))) {
+            stop("the '", names(K)[i], "' data table must be columns named.", 
                 call.=FALSE)
         }
     }
     
-    ##- strata is named vector
-    if (is.null(names(strata))) {
-        stop("'strata' must be a named vector.", call.=FALSE)
-    }
+    ##- samples are named correctly
+    cnames <- rownames(colData)
     
-    ##- samples are ordered correctly
-    rnames <- names(strata)
     for (i in seq_along(K)) {
-        if (!identical(rnames, rownames(K[[i]]))) {
-            stop("non equal row names among tables and/or strata", 
-                call.=FALSE)
+        if (!(all(colnames(K[[i]]) %in% cnames))) {
+            stop("columns names in '", names(K)[i], "' data table do not ",
+                "match row names in colData.", call.=FALSE)
         }
     }
     
-    strata <- as.factor(strata)
+    ##- missing individuals
+    miss <- list()
     
-    ##- there are no missing individuals
-    miss <- lapply(K, function(X) {
-            rownames(X)[which(apply(is.na(X), 1, function(x) { all(x) }))] })
+    for (i in seq_along(K)) {
+        idx <- !(cnames %in% colnames(K[[i]]))
+        miss[[i]] <- cnames[idx]
+    }
     
-    if (length(unlist(miss)) == 0)
-        stop("no missing rows in the data tables, MI is not useful.", 
-            " Perform MFA.", call.=FALSE)
-    
+    names(miss) <- names(K)
     miss <- miss[vapply(miss, length, 1L) > 0]
     
     ##- end checking ---------------------------------------------------------#
     
     
-    ##- the S4 class ---------------------------------------------------------#
+    ##- the MIDTList S4 class ------------------------------------------------#
     ##------------------------------------------------------------------------#
-    object <- MIDTList(incompleteData=K, strata=strata, 
-                        tableNames=tableNames, missingRows=miss)
-    return(object)
+    midt <- MultiAssayExperiment(experiments=K, colData=colData)
+    midt <- new("MIDTList", midt, strata=strata, missingIndv=miss)
+    
+    return(midt)
+}
+
+
+##- newMIDTList method for MultiAssayExperiment class ------------------------#
+##----------------------------------------------------------------------------#
+newMIDTList.MultiAssayExperiment <- function(object, strata=NULL, ...) {
+    
+    ##- initialization of variables ------------------------------------------#
+    ##------------------------------------------------------------------------#
+    K <- assays(object)
+    
+    ##- strata
+    if (is.null(strata) & (ncol(colData(object)) == 1)) {
+        strata <- colnames(colData(object))
+        
+        if (is.null(strata)) {
+            strata <- colnames(colData(object)) <- "strata"
+        }
+    }
+    
+    if (length(strata) > 1) {
+        stop("'strata' must be a vector of length 1.", call.=FALSE)
+    }
+    
+    if (is.character(strata)) {
+        cond <- (strata %in% colnames(colData(object)))
+        
+        if (!cond || length(cond) == 0) {
+            stop("'strata' do not match colnames in colData.", call.=FALSE)
+        }
+    } else {
+        if (strata %in% seq_len(ncol(colData(object)))) {
+            stop("'strata' must be a integer value less than or equal to ", 
+                ncol(colData(object)), call.=FALSE)
+        }
+    }
+    
+    cnames <- rownames(colData(object))
+    
+    ##- the individual tables
+    for (i in seq_along(K)) {
+        if (length(dim(K[[i]])) != 2) {
+            stop("the '", names(K)[i], "' data table must be a matrix or",
+                " data frame.", call.=FALSE)
+        }
+        
+        K[[i]] <- as.matrix(K[[i]])
+        
+        if (!is.numeric(K[[i]])) {
+            stop("the '", names(K)[i], "' data table must be a matrix or",
+                " data frame.", call.=FALSE)
+        }
+    }
+    
+    ### Inf values in tables
+    for (i in seq_along(K)) {
+        if (any(apply(K[[i]], 1, is.infinite))) {
+            stop("infinite values in '", names(K)[i], "' data table.", 
+                call.=FALSE)
+        }
+    }
+    
+    ##- missing individuals
+    miss <- vector("list", length = length(K))
+    names(miss) <- names(K)
+    mapList <- mapToList(sampleMap(object), "assay")
+    
+    for (i in names(K)) {
+        idx <- !(cnames %in% mapList[[i]]$primary)
+        miss[[i]] <- cnames[idx]
+    }
+    
+    miss <- miss[vapply(miss, length, 1L) > 0]
+
+    
+    ##- the MIDTList S4 class ------------------------------------------------#
+    ##------------------------------------------------------------------------#
+    midt <- new("MIDTList", object, strata=strata, missingIndv=miss)
+    
+    return(midt)
 }
