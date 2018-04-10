578615d0af624005145c719ed2d0dd803d8ea5ab
Ignacio <ignacio.gonzalez@bbox.fr>
2018-04-10 11:26:23 +0200

    Updating test.

--- tests/testthat/test-MIDTList.R
+++ tests/testthat/test-MIDTList.R
@@ -6,7 +6,7 @@ test_that("MIDTList object from a list of data tables", {
     tableList <- NCI60$dataTables[1:2]
     colData <- NCI60$dataTables$cell.line
     
-    midt <- newMIDTList(tableList, colData=colData, 
+    midt <- MIDTList(tableList, colData=colData, 
                         assayNames=c("trans", "prote"))
     expect_is(midt, "MIDTList")
 })
@@ -16,13 +16,13 @@ test_that("MIDTList object from separate data tables", {
     table2 <- NCI60$dataTables$prote
     colData <- NCI60$dataTables$cell.line
     
-    midt <- newMIDTList(table1, table2, colData=colData,
+    midt <- MIDTList(table1, table2, colData=colData,
                         assayNames=c("transcrip", "proteome"))
     expect_is(midt, "MIDTList")
 })
 
 test_that("MIDTList object from 'MultiAssayExperiment'", {
-    midt <- newMIDTList(NCI60$mae)
+    midt <- MIDTList(NCI60$mae)
     expect_is(midt, "MIDTList")
 })
 
@@ -34,19 +34,19 @@ test_that("tests for data tables", {
     table2 <- NCI60$dataTables$prote
     colData <- NCI60$dataTables$cell.line
     
-    expect_error(newMIDTList(table1, colData=colData), 
+    expect_error(MIDTList(table1, colData=colData), 
             "at least two data tables must be passed as arguments in '...'")
     
     tableNrn <- table1
     colnames(tableNrn) <- NULL
-    expect_error(newMIDTList(tableNrn, table2, colData=colData), 
+    expect_error(MIDTList(tableNrn, table2, colData=colData), 
                 "the 'Table 1' data table must be columns named.")
     
-    expect_error(newMIDTList(1:nrow(table1), table2, colData=colData), 
+    expect_error(MIDTList(1:nrow(table1), table2, colData=colData), 
                 "the 'Table 1' data table must be a matrix or data frame.")
     
     tableInf <- table1
     tableInf[1, 1] <- Inf
-    expect_error(newMIDTList(tableInf, table2, colData=colData), 
+    expect_error(MIDTList(tableInf, table2, colData=colData), 
                  "infinite values in 'Table 1' data table.")
 })
